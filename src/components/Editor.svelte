01488becfa82b81777d2e45e5c3c1f2138ae1fea
Ahmed Morsi <eng.eramax@gmail.com>
2025-03-30 01:39:59 +0100

    fix monaco-editor

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,11 +1,51 @@
 <script>
     import { files, activeFile } from "../lib/store";
+    import { onMount } from "svelte";
+    import loader from "@monaco-editor/loader";
+
+    let editorContainer;
+    let editor;
+
+    $effect(() => {
+        if (editor && $files[$activeFile] !== editor.getValue()) {
+            editor.setValue($files[$activeFile]);
+        }
+    });
+
+    onMount(async () => {
+        const monaco = await loader.init();
+        console.log("Monaco Editor loaded", monaco);
+        monaco.languages.html.htmlDefaults.setOptions({
+            format: {
+                tabSize: 2,
+                insertSpaces: true,
+                wrapAttributes: "auto",
+                wrapAttributesIndentSize: 2,
+            },
+        });
+        editor = monaco.editor.create(editorContainer, {
+            value: $files[$activeFile],
+            language: "html",
+            theme: "vs-dark",
+            automaticLayout: true,
+            minimap: { enabled: true },
+            lineNumbers: "on",
+            formatOnPaste: true,
+            formatOnType: true,
+            scrollBeyondLastLine: false,
+        });
+
+        editor.onDidChangeModelContent(() => {
+            const newValue = editor.getValue();
+            if ($files[$activeFile] !== newValue) {
+                $files[$activeFile] = newValue;
+            }
+        });
+
+        return () => editor?.dispose();
+    });
 </script>
 
 <div class="flex-1 bg-app-darker">
-    <textarea
-        class="w-full h-full bg-transparent text-white p-4 font-mono focus:outline-none"
-        bind:value={$files[$activeFile]}
-    ></textarea>
+    <div bind:this={editorContainer} class="w-full h-full" />
 </div>
-
