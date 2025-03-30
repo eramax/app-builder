16aae8a7b97fc8cb7e41b599da8f5e93325fb2ea
Ahmed Morsi <eng.eramax@gmail.com>
2025-03-30 23:04:12 +0200

    update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,11 +14,11 @@
   <div class="h-screen flex flex-col">
     <div class="grid grid-cols-2 gap-4 flex-grow h-full p-4">
       <div class="flex flex-col gap-4 h-full">
-        <Prompt />
+        
         <div class="bg-[#1e1e1e] rounded-lg flex flex-col flex-grow overflow-hidden">
           <Navigation />
           <Editor />
-        </div>
+        </div><Prompt />
       </div>
       <div class="bg-white rounded-lg overflow-hidden">
         <Preview />
